9938819c7706b9f1e2ed06bb4febe21e0bd13714
ryder <rbbar271@learner.dvusd.org>
2024-01-05 09:30:32 -0700

    the actual first project
    converter project

---
+++ firstProjectConverter/firstProjectConverter/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int i = 0; i < 101; i++)
+{
+    Console.WriteLine(i);
+}
+Console.ReadKey();
