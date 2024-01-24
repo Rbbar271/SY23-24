cc914856d1542e7cc83b5951b0323562bfba2895
ryder <rbbar271@learner.dvusd.org>
2024-01-24 08:56:29 -0700

    pokedex update 01,24,24

--- pokedex/pokedex/Form1.Designer.cs
+++ pokedex/pokedex/Form1.Designer.cs
@@ -47,11 +47,16 @@
             this.type2label = new System.Windows.Forms.Label();
             this.AttacktextBox = new System.Windows.Forms.TextBox();
             this.Attacklabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox = new System.Windows.Forms.TextBox();
             this.savebutton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.debugBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.maxbutton = new System.Windows.Forms.Button();
+            this.nonmaxbutton = new System.Windows.Forms.Button();
+            this.sp_attackbutton = new System.Windows.Forms.Button();
+            this.sp_deffencebutton = new System.Windows.Forms.Button();
+            this.attackbutton = new System.Windows.Forms.Button();
+            this.deffencebutton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,9 +77,9 @@
             // 
             // typetextBox
             // 
-            this.typetextBox.Location = new System.Drawing.Point(188, 227);
+            this.typetextBox.Location = new System.Drawing.Point(188, 179);
             this.typetextBox.Name = "typetextBox";
-            this.typetextBox.Size = new System.Drawing.Size(69, 22);
+            this.typetextBox.Size = new System.Drawing.Size(146, 22);
             this.typetextBox.TabIndex = 2;
             // 
             // namelabel
@@ -89,7 +94,7 @@
             // typelabel
             // 
             this.typelabel.AutoSize = true;
-            this.typelabel.Location = new System.Drawing.Point(185, 209);
+            this.typelabel.Location = new System.Drawing.Point(185, 161);
             this.typelabel.Name = "typelabel";
             this.typelabel.Size = new System.Drawing.Size(39, 16);
             this.typelabel.TabIndex = 4;
@@ -181,23 +186,24 @@
             // 
             // type2textBox
             // 
-            this.type2textBox.Location = new System.Drawing.Point(265, 227);
+            this.type2textBox.Location = new System.Drawing.Point(188, 228);
             this.type2textBox.Name = "type2textBox";
-            this.type2textBox.Size = new System.Drawing.Size(69, 22);
+            this.type2textBox.Size = new System.Drawing.Size(146, 22);
             this.type2textBox.TabIndex = 15;
             // 
             // type2label
             // 
             this.type2label.AutoSize = true;
-            this.type2label.Location = new System.Drawing.Point(263, 209);
+            this.type2label.Location = new System.Drawing.Point(186, 210);
             this.type2label.Name = "type2label";
-            this.type2label.Size = new System.Drawing.Size(39, 16);
+            this.type2label.Size = new System.Drawing.Size(49, 16);
             this.type2label.TabIndex = 16;
-            this.type2label.Text = "Type";
+            this.type2label.Text = "Type 2";
             // 
             // AttacktextBox
             // 
-            this.AttacktextBox.Location = new System.Drawing.Point(188, 179);
+            this.AttacktextBox.Enabled = false;
+            this.AttacktextBox.Location = new System.Drawing.Point(561, 273);
             this.AttacktextBox.Name = "AttacktextBox";
             this.AttacktextBox.Size = new System.Drawing.Size(146, 22);
             this.AttacktextBox.TabIndex = 17;
@@ -205,30 +211,11 @@
             // Attacklabel
             // 
             this.Attacklabel.AutoSize = true;
-            this.Attacklabel.Location = new System.Drawing.Point(185, 160);
+            this.Attacklabel.Location = new System.Drawing.Point(558, 254);
             this.Attacklabel.Name = "Attacklabel";
             this.Attacklabel.Size = new System.Drawing.Size(44, 16);
             this.Attacklabel.TabIndex = 18;
             this.Attacklabel.Text = "Attack";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(36, 255);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 50);
-            this.button1.TabIndex = 21;
-            this.button1.Text = "<";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox
-            // 
-            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox.Location = new System.Drawing.Point(92, 256);
-            this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(50, 49);
-            this.textBox.TabIndex = 23;
-            this.textBox.Text = "0";
-            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // savebutton
             // 
@@ -240,33 +227,114 @@
             this.savebutton.UseVisualStyleBackColor = true;
             this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(148, 256);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 50);
-            this.button2.TabIndex = 25;
-            this.button2.Text = ">";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // debugBox
             // 
+            this.debugBox.Enabled = false;
             this.debugBox.Location = new System.Drawing.Point(340, 93);
             this.debugBox.Multiline = true;
             this.debugBox.Name = "debugBox";
             this.debugBox.Size = new System.Drawing.Size(367, 157);
             this.debugBox.TabIndex = 26;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 40);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "mono type";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(188, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 40);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "duel type";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // maxbutton
+            // 
+            this.maxbutton.Location = new System.Drawing.Point(36, 257);
+            this.maxbutton.Name = "maxbutton";
+            this.maxbutton.Size = new System.Drawing.Size(69, 55);
+            this.maxbutton.TabIndex = 29;
+            this.maxbutton.Text = "max level";
+            this.maxbutton.UseVisualStyleBackColor = true;
+            this.maxbutton.Click += new System.EventHandler(this.maxbutton_Click);
+            // 
+            // nonmaxbutton
+            // 
+            this.nonmaxbutton.Location = new System.Drawing.Point(36, 254);
+            this.nonmaxbutton.Name = "nonmaxbutton";
+            this.nonmaxbutton.Size = new System.Drawing.Size(69, 56);
+            this.nonmaxbutton.TabIndex = 30;
+            this.nonmaxbutton.Text = "not max level";
+            this.nonmaxbutton.UseVisualStyleBackColor = true;
+            this.nonmaxbutton.Visible = false;
+            this.nonmaxbutton.Click += new System.EventHandler(this.nonmaxbutton_Click);
+            // 
+            // sp_attackbutton
+            // 
+            this.sp_attackbutton.Location = new System.Drawing.Point(561, 301);
+            this.sp_attackbutton.Name = "sp_attackbutton";
+            this.sp_attackbutton.Size = new System.Drawing.Size(145, 23);
+            this.sp_attackbutton.TabIndex = 31;
+            this.sp_attackbutton.Text = "SP_attack";
+            this.sp_attackbutton.UseVisualStyleBackColor = true;
+            this.sp_attackbutton.Click += new System.EventHandler(this.sp_attackbutton_Click);
+            // 
+            // sp_deffencebutton
+            // 
+            this.sp_deffencebutton.Location = new System.Drawing.Point(561, 330);
+            this.sp_deffencebutton.Name = "sp_deffencebutton";
+            this.sp_deffencebutton.Size = new System.Drawing.Size(145, 23);
+            this.sp_deffencebutton.TabIndex = 32;
+            this.sp_deffencebutton.Text = "SP_deffence";
+            this.sp_deffencebutton.UseVisualStyleBackColor = true;
+            this.sp_deffencebutton.Click += new System.EventHandler(this.sp_deffencebutton_Click);
+            // 
+            // attackbutton
+            // 
+            this.attackbutton.Location = new System.Drawing.Point(561, 359);
+            this.attackbutton.Name = "attackbutton";
+            this.attackbutton.Size = new System.Drawing.Size(145, 23);
+            this.attackbutton.TabIndex = 33;
+            this.attackbutton.Text = "attack";
+            this.attackbutton.UseVisualStyleBackColor = true;
+            this.attackbutton.Click += new System.EventHandler(this.attackbutton_Click);
+            // 
+            // deffencebutton
+            // 
+            this.deffencebutton.Location = new System.Drawing.Point(561, 388);
+            this.deffencebutton.Name = "deffencebutton";
+            this.deffencebutton.Size = new System.Drawing.Size(145, 23);
+            this.deffencebutton.TabIndex = 34;
+            this.deffencebutton.Text = "deffence";
+            this.deffencebutton.UseVisualStyleBackColor = true;
+            this.deffencebutton.Click += new System.EventHandler(this.deffencebutton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deffencebutton);
+            this.Controls.Add(this.attackbutton);
+            this.Controls.Add(this.sp_deffencebutton);
+            this.Controls.Add(this.sp_attackbutton);
+            this.Controls.Add(this.nonmaxbutton);
+            this.Controls.Add(this.maxbutton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.debugBox);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.savebutton);
-            this.Controls.Add(this.textBox);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.Attacklabel);
             this.Controls.Add(this.AttacktextBox);
             this.Controls.Add(this.type2label);
@@ -286,6 +354,7 @@
             this.Controls.Add(this.typetextBox);
             this.Controls.Add(this.nametextBox);
             this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -316,11 +385,16 @@
         private System.Windows.Forms.Label type2label;
         private System.Windows.Forms.TextBox AttacktextBox;
         private System.Windows.Forms.Label Attacklabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button savebutton;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox debugBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button maxbutton;
+        private System.Windows.Forms.Button nonmaxbutton;
+        private System.Windows.Forms.Button sp_attackbutton;
+        private System.Windows.Forms.Button sp_deffencebutton;
+        private System.Windows.Forms.Button attackbutton;
+        private System.Windows.Forms.Button deffencebutton;
     }
 }
 
--- pokedex/pokedex/Form1.cs
+++ pokedex/pokedex/Form1.cs
@@ -11,19 +11,19 @@ using System.Windows.Forms;
 
 namespace pokedex
 {
-    enum attacks{sp_attck, sp_deffence, attack, deffence}
+    enum attacks{sp_attack, sp_deffence, attack, deffence}
     struct pokemon
     {
-        string Name;
-        string Type;
-        string Type2;
-        int Level;
-        attacks Attacktype;
-        int HP;
-        int EXP;
-        bool Legandery;
-        bool Shiny;
-        int Gen;
+        public string Name;
+        public string Type;
+        public string Type2;
+        public int Level;
+        public attacks Attacktype;
+        public int HP;
+        public int EXP;
+        public bool Legandery;
+        public bool Shiny;
+        public int Gen;
     }
     public partial class Form1 : Form
     {
@@ -39,53 +39,177 @@ namespace pokedex
             {
                 StreamReader infile = new StreamReader("pokemon.txt");
                 string s = infile.ReadToEnd();
+                pokemon p = readpokemon(s);
+                showpokemon(p);
                 infile.Close();
             }
         }
+        private pokemon readpokemon(string s)
+        {
+            pokemon p = new pokemon();
+            string[] feilds = s.Split('|');
+            p.Name = feilds[0];
+            p.Type = feilds[6];
+            p.Level = int.Parse(feilds[1]);
+            p.Attacktype = (attacks)Enum.Parse(typeof(attacks), feilds[3]);
+            p.HP = int.Parse(feilds[2]);
+            p.EXP = int.Parse(feilds[4]);
 
+            if (feilds[8] == "True")
+                p.Legandery = true;
+            else
+                p.Legandery = false;
+            if (feilds[7] == "True")
+                p.Shiny = true;
+            else
+                p.Shiny = false;
+            p.Gen = int.Parse(feilds[5]);
+            p.Type2 = feilds[9];
+            return p;
+
+        }
         private void savebutton_Click(object sender, EventArgs e)
         {
             debugBox.Clear();
-            debugBox.Text += " " + namelabel.Text + ": ";
-            debugBox.Text += " " + nametextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + levellabel.Text + ": ";
-            debugBox.Text += " " + leveltextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + hplabel.Text + ": ";
-            debugBox.Text += " " + hptextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + Attacklabel.Text + ": ";
-            debugBox.Text += " " + AttacktextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + explabel.Text + ": ";
-            debugBox.Text += " " + exptextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + genlabel.Text + ": ";
-            debugBox.Text += " " + gentextBox.Text + " " ;
-            debugBox.Text += " | ";
-            debugBox.Text += " " + typelabel.Text + ": ";
-            debugBox.Text += " " + typetextBox.Text + " " ;
-            if (type2textBox != null && type2textBox.Text != "nune")
-            {
-                debugBox.Text += " | ";
-                debugBox.Text += " " + type2label.Text + ": ";
-                debugBox.Text += " " + type2textBox.Text + " ";
-            }
+            
+            debugBox.Text +=  nametextBox.Text;
+            debugBox.Text += "|";
+            
+            debugBox.Text +=  leveltextBox.Text ;
+            debugBox.Text += "|";
+            
+            debugBox.Text +=  hptextBox.Text ;
+            debugBox.Text += "|";
+            
+            debugBox.Text += AttacktextBox.Text;
+            debugBox.Text += "|";
+            
+            debugBox.Text += exptextBox.Text;
+            debugBox.Text += "|";
+            
+            debugBox.Text += gentextBox.Text;
+            debugBox.Text += "|";
+            
+            debugBox.Text += typetextBox.Text;
+            
             if (shinycheckBox.Checked)
             {
-                debugBox.Text += " | ";
-                debugBox.Text += " shiny ";
+                debugBox.Text += "|";
+                debugBox.Text += "shiny";
+            }
+            else
+            {
+                debugBox.Text += "|";
+                debugBox.Text += "none shiny";
             }
             if (leganderycheckBox.Checked)
             {
-                debugBox.Text += " | ";
-                debugBox.Text += " legandery ";
+                debugBox.Text += "|";
+                debugBox.Text += "legandery";
+            }
+            else
+            {
+                debugBox.Text += "|";
+                debugBox.Text += "none legandery";
+            }
+            if (type2textBox.Visible == true)
+            {
+                debugBox.Text += "|";
+                
+                debugBox.Text += type2textBox.Text;
+            }
+            else
+            {
+                debugBox.Text += "|";
+                debugBox.Text += "monotype";
             }
 
             StreamWriter outfile = new StreamWriter("pokemon.txt");
             outfile.WriteLine(debugBox.Text);
             outfile.Close();
         }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            type2label.Visible = false;
+            type2textBox.Visible = false;
+            button4.Visible = true;
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            type2label.Visible = true;
+            type2textBox.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void nonmaxbutton_Click(object sender, EventArgs e)
+        {
+            leveltextBox.Enabled = true;
+            leveltextBox.Text = "";
+            exptextBox.Enabled = true;
+            exptextBox.Text = "";
+            maxbutton.Visible = true;
+            nonmaxbutton.Visible = false;
+        }
+
+        private void maxbutton_Click(object sender, EventArgs e)
+        {
+            leveltextBox.Text = "100";
+            leveltextBox.Enabled = false;
+            exptextBox.Text = "0";
+            exptextBox.Enabled = false;
+            maxbutton.Visible = false;
+            nonmaxbutton.Visible = true;
+        }
+       
+        private void sp_attackbutton_Click(object sender, EventArgs e)
+        {
+            AttacktextBox.Text = "";
+            AttacktextBox.Text = "sp_attack";
+            sp_attackbutton.Enabled = false;
+            sp_deffencebutton.Enabled = true;
+            attackbutton.Enabled = true;
+            deffencebutton.Enabled = true;
+        }
+
+        private void sp_deffencebutton_Click(object sender, EventArgs e)
+        {
+            AttacktextBox.Text = "";
+            AttacktextBox.Text = "sp_deffence";
+            sp_attackbutton.Enabled = true;
+            sp_deffencebutton.Enabled = false;
+            attackbutton.Enabled = true;
+            deffencebutton.Enabled = true;
+        }
+
+        private void attackbutton_Click(object sender, EventArgs e)
+        {
+            AttacktextBox.Text = "";
+            AttacktextBox.Text = "attack";
+            sp_attackbutton.Enabled = true;
+            sp_deffencebutton.Enabled = true;
+            attackbutton.Enabled = false;
+            deffencebutton.Enabled = true;
+        }
+
+        private void deffencebutton_Click(object sender, EventArgs e)
+        {
+            AttacktextBox.Text = "";
+            AttacktextBox.Text = "deffence";
+            sp_attackbutton.Enabled = true;
+            sp_deffencebutton.Enabled = true;
+            attackbutton.Enabled = true;
+            deffencebutton.Enabled = false;
+        }
+        void showpokemon(pokemon p)
+        {
+            nametextBox.Text = p.Name;
+
+        }
+
     }
 }
